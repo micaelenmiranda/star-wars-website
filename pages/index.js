7f4e7021ddc8629f2f69b90528723937b3fac773
micaelenmiranda <mirandamicaelen@gmail.com>
2021-11-12 21:46:34 -0300

    build: adiciona estrutura inicial

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>Star Wars franchise</title>
+        <meta name="description" content="A website dedicated to the Star Wars franchise" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default IndexPage
